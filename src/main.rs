e3f5419193b690fc62fd1914ac811ea5cdd71b5a
Austin Taylor Mann <Fancysause@gmail.com>
2019-11-21 22:43:13 -0500

    set up main function to take user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    use std::io::{stdin,stdout,Write};
+    let mut s =String::new();
+    print!("Please enter some text: ");
+    let _=stdout().flush();
+    stdin().read_line(&mut s).expect("Did not enter a correct string");
+    if let Some('\n')=s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r')=s.chars().next_back() {
+        s.pop();
+    }
+    println!("You typed: {}",s);
 }
